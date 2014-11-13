0f0a61f8c9c108c202c6370ac435404d09c6342c
Andrew Purtell <apurtell@apache.org>
2014-11-12 16:39:16 -0800

    Use HBase 0.98.8

--- bin/env.sh
+++ bin/env.sh
@@ -14,7 +14,8 @@ export HADOOP_URL=http://hbase.s3.amazonaws.com/hadoop/hadoop-${HADOOP_VERSION}.
 
 #export HBASE_VERSION=0.98.0
 #export HBASE_VERSION=0.98.6.1
-export HBASE_VERSION=0.98.7
+#export HBASE_VERSION=0.98.7
+export HBASE_VERSION=0.98.8
 export HBASE_URL=http://hbase.s3.amazonaws.com/hbase/hbase-hadoop-${HADOOP_VERSION}/hbase-${HBASE_VERSION}-bin.tar.gz
 
 # YCSB distribution tarball location
