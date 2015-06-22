c665163a24e529e930fe3ee047d92e94e2317ff0
Andrew Purtell <apurtell@apache.org>
2015-06-22 12:41:03 -0700

    Add Spark
    Also, clean up versions in env.sh

--- bin/env.sh
+++ bin/env.sh
@@ -6,14 +6,12 @@ REGION=${REGION:-us-west-2}
 
 # The version of Hadoop to use and the distribution tarball location
 
-export HADOOP_VERSION=2.5.2
-#export HADOOP_VERSION=2.2.0
+export HADOOP_VERSION=2.6.0
 export HADOOP_URL=http://hbase.s3.amazonaws.com/hadoop/hadoop-${HADOOP_VERSION}.tar.gz
 
 # The version of HBase to use and the distribution tarball location
 
-export HBASE_VERSION=0.98.12
-#export HBASE_VERSION=0.98.0
+export HBASE_VERSION=0.98.13
 export HBASE_URL=http://hbase.s3.amazonaws.com/hbase/hbase-hadoop-${HADOOP_VERSION}/hbase-${HBASE_VERSION}-bin.tar.gz
 
 # YCSB distribution tarball location
@@ -24,6 +22,11 @@ export YCSB_URL=http://hbase.s3.amazonaws.com/hbase/hbase-hadoop-${HADOOP_VERSIO
 
 export PHOENIX_VERSION=4.3.1
 export PHOENIX_URL=http://hbase.s3.amazonaws.com/phoenix/phoenix-hadoop-${HADOOP_VERSION}-hbase-${HBASE_VERSION}/phoenix-${PHOENIX_VERSION}.tar.gz
+
+# The version of Spark to use and the distribution tarball location
+
+export SPARK_VERSION=1.4.0
+export SPARK_URL=http://hbase.s3.amazonaws.com/spark/spark-hadoop-${HADOOP_VERSION}/spark-${SPARK_VERSION}-bin.tar.gz
 
 ############################################################################
 
@@ -41,6 +44,5 @@ case $REGION in
 esac
 
 #JAVA_VERSION=7u79
-#JAVA_VERSION=8u20
 JAVA_VERSION=8u45
 JAVA_URL=http://hbase.s3.amazonaws.com/jdk/jdk-${JAVA_VERSION}-linux-x64.tar.gz
