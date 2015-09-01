dcf6778171fcfdda9344b267a8f2f0c44613861f
Andrew Purtell <apurtell@apache.org>
2015-09-01 12:56:26 -0700

    Update software versions to latest releases

--- bin/env.sh
+++ bin/env.sh
@@ -6,12 +6,12 @@ REGION=${REGION:-us-west-2}
 
 # The version of Hadoop to use and the distribution tarball location
 
-export HADOOP_VERSION=2.6.0
+export HADOOP_VERSION=2.7.1
 export HADOOP_URL=http://hbase.s3.amazonaws.com/hadoop/hadoop-${HADOOP_VERSION}.tar.gz
 
 # The version of HBase to use and the distribution tarball location
 
-export HBASE_VERSION=0.98.13
+export HBASE_VERSION=0.98.14
 export HBASE_URL=http://hbase.s3.amazonaws.com/hbase/hbase-hadoop-${HADOOP_VERSION}/hbase-${HBASE_VERSION}-bin.tar.gz
 
 # YCSB distribution tarball location
@@ -20,12 +20,12 @@ export YCSB_URL=http://hbase.s3.amazonaws.com/hbase/hbase-hadoop-${HADOOP_VERSIO
 
 # The version of Phoenix to use and the distribution tarball location
 
-export PHOENIX_VERSION=4.3.1
+export PHOENIX_VERSION=4.5.1
 export PHOENIX_URL=http://hbase.s3.amazonaws.com/phoenix/phoenix-hadoop-${HADOOP_VERSION}-hbase-${HBASE_VERSION}/phoenix-${PHOENIX_VERSION}.tar.gz
 
 # The version of Spark to use and the distribution tarball location
 
-export SPARK_VERSION=1.4.0
+export SPARK_VERSION=1.4.1
 export SPARK_URL=http://hbase.s3.amazonaws.com/spark/spark-hadoop-${HADOOP_VERSION}/spark-${SPARK_VERSION}-bin.tar.gz
 
 ############################################################################
